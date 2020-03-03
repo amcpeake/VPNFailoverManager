080900f2876dcfc103979ea5a3645df18957caf7
Aidan McPeake <46015459+amcpeake@users.noreply.github.com>
2020-03-03 14:08:16 -0500

    Updated to allow PIA connections and Thomas' VPN connections

--- defSwitch.sh
+++ defSwitch.sh
@@ -24,10 +24,18 @@ elif [ "$1" == "active" ]; then
 elif [ "$1" == "list" ]; then
 	echo "Available interfaces: $(listInts)"
 	exit 0
-
 elif [[ $EUID -ne 0 ]]; then
 	echo "Changing interface requires sudo"
 	exit 1
+
+elif [ "$1" == "clear" ]; then
+	if (ip route delete 0.0.0.0/1 &> /dev/null) && (ip route delete 128.0.0.0/1 &>/dev/null); then
+		echo "Successfully cleared tunnel configuration"
+		exit 0
+	else
+		echo "Failed to clear tunnel configuration"
+		exit 1
+	fi
 
 elif !(echo "$(listInts)" | grep -q "$1"); then # If input not in available interfaces
 	echo "Interface \"$1\" is not valid"
@@ -35,13 +43,19 @@ elif !(echo "$(listInts)" | grep -q "$1"); then # If input not in available inte
 	exit 1
 
 else
-	echo $1
-	ipv4="$(ip address show dev "$1" | grep "inet " | awk '{print $2}' | cut -d"/" -f1 | awk -F'.' '{print $1"."$2"."$3".1"}')"
-
+	ipv4="$(ip address show dev "$1" | grep "inet ")"
+	if (echo "$ipv4" | grep -q "peer"); then
+		ipv4="$(echo "$ipv4" | awk '{print $4}' | cut -d"/" -f1)"
+	else
+		ipv4="$(echo "$ipv4" | awk '{print $2}' | cut -d"/" -f1 | awk -F'.' '{print $1"."$2"."$3".1"}')"
+	fi
+	
 	if (ip route replace 0.0.0.0/1 via "$ipv4" dev "$1" &> /dev/null) && (ip route replace 128.0.0.0/1 via "$ipv4" dev "$1" &> /dev/null); then
 		echo "Successfully set interface to $1"
+		exit 0
 	else
 		echo "Failed to set interface to $1"
+		exit 1
 	fi	
 fi
 
