a15b998243cfe35b99651d6984b2105df163c059
yuenster <56559748+yuenster@users.noreply.github.com>
2020-03-05 16:35:06 -0800

    Add files via upload
    Struggling to figure out why speedtest-cli doesn't allow me to loop through IPs.
    Line 32 of getNetStats.py fails on the second loop but works fine on the first.
    If you change "source_address=ip" to "source_address=IP[0]" aka make the ip static, it runs fine both times.

--- getNetStats.py
+++ getNetStats.py
@@ -1,6 +1,7 @@
 import os
 import re
 import json
+import speedtest
 
 
 def getIP():
@@ -21,16 +22,23 @@ def getIP():
 
 
 def main():
+
     IP = getIP()
     with open('rtt.txt', 'w') as r:
         jsonList = []
         for i, ip in enumerate(IP):
             os.system('ping %s -c 4 > latency.txt' % ip)
+            #The following line doesn't work on the second for loop, if you replace source_address=ip with source_address=IP[0], then it runs fine both times. Only when you change the source_address it fails
+            st = speedtest.Speedtest(source_address=ip)
+            st.get_best_server(st.set_mini_server("http://speedtest.asbn.va.wtsky.net"))
+            up = st.upload()
+            down = st.download()
 
             with open('latency.txt') as f:
                 rttList = []
                 lines = f.readlines()
                 print(lines[-1])
+                print("Download: {} bits/s, Upload: {} bits/s".format(int(up), int(down)))
                 rttList.append(lines[-1])
                 rtt = rttList[0].split("/")
 
@@ -39,7 +47,9 @@ def main():
                     'minRTT': rtt[3][-5:],
                     'avgRTT': rtt[4],
                     'maxRTT': rtt[5],
-                    'mDEV': rtt[6][:5]
+                    'mDEV': rtt[6][:5],
+                    'UploadBW': up,
+                    'DownloadBW': down
                 })
 
                 jsonList.append(data)
