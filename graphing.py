eca062d3ebc7b71d505b788bb5ecf1f83d4f452e
Thomas Yungblut <sifotes@hotmail.com>
2020-03-21 12:55:12 -0400

    Improved graphing, doesn't properly show multiple x-axis scales / labels.

--- graphing.py
+++ graphing.py
@@ -1,5 +1,5 @@
 import json
-import pandas as pd
+#import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -7,16 +7,25 @@ import matplotlib.pyplot as plt
 with open('testData.json', 'r') as testData:
 	data=json.load(testData)
 
+
+# Get all data from JSON file.
 dates = [i['time'] for i in data["data"]]
 latency = [i['latency'] for i in data['data']]
 download = [i['download'] for i in data['data']]
 upload = [i['upload'] for i in data['data']]
 
+plt.plot(dates, latency, label='Latency')
 
+plt.plot(dates, download, label='Download')
 
+plt.plot(dates, upload, label='Upload')
 
-df = pd.DataFrame({'dates':dates, 'latency':latency})
-df['dates']  = [pd.to_datetime(i) for i in df['dates']]
+# Labeling the graph.
+plt.xlabel('Timestamp')
+plt.ylabel('Speed (mpbs)')
 
-plt.bar(dates, latency)
+plt.title('Recorded Statistics')
+plt.legend()
+
+# Show the graph
 plt.show()
