9558b558e195a29f5bbefb0b8eeb4150693722b1
yuenster <56559748+yuenster@users.noreply.github.com>
2020-03-22 20:39:49 -0400

    bw now shows up to 3 decimal places and fixed rtt

--- getNetStats.py
+++ getNetStats.py
@@ -27,25 +27,24 @@ def main():
     with open('output.txt', 'w') as r:
         jsonList = []
         for i, ip in enumerate(IP):
-            os.system('ping %s -c 4 > ping.txt' % ip)
-
+            os.system('ping 8.8.8.8 -c 4 -I tun%s > ping.txt' % i)
 
             start_timeDn = time.time()
-            os.system('sudo /home/oyuen/PycharmProjects/getNetStats/wget.sh --interface tun% down' % i)
+            os.system('sudo /home/oyuen/PycharmProjects/getNetStats/wget.sh --interface tun%s down' % i)
             # os.system('sudo /home/oyuen/PycharmProjects/getNetStats/wget.sh --interface tun0 down')
             end_timeDn = time.time()
             total_timeDn = end_timeDn - start_timeDn
-            rounded_timeDn = "%.3f" % round(total_timeDn, 3)
-            dl_bw = float(100) / float(rounded_timeDn)
+            dl_bw = float(100) / float(total_timeDn)
+            dl_bw = "%.3f" % round(dl_bw, 3)
             print('Download Bandwidth: ', dl_bw, 'mbps')
 
             start_timeUp = time.time()
-            os.system('sudo /home/oyuen/PycharmProjects/getNetStats/wget.sh --interface tun% up' % i)
+            os.system('sudo /home/oyuen/PycharmProjects/getNetStats/wget.sh --interface tun%s up' % i)
             # os.system('sudo /home/oyuen/PycharmProjects/getNetStats/wget.sh --interface tun0 up')
             end_timeUp = time.time()
             total_timeUp = end_timeUp - start_timeUp
-            rounded_timeUp = "%.3f" % round(total_timeUp, 3)
-            up_bw = float(100) / float(rounded_timeUp)
+            up_bw = float(100) / float(total_timeUp)
+            up_bw = "%.3f" % round(up_bw, 3)
             print('Upload Bandwidth: ', up_bw, 'mbps')
 
             with open('ping.txt') as f:
