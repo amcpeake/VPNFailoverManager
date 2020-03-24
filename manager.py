9a50720b65077ccd7e3b66b127ffe5f0e0455857
Aidan McPeake <aidanmcpeake9@gmail.com>
2020-03-23 21:05:03 -0400

    Refactored measuring/control via agent/manager.py

---
+++ manager.py
@@ -0,0 +1,28 @@
+import agent
+import graphing
+import json
+from datetime import datetime
+import subprocess
+
+
+def runCMD(cmd): # Run a command and return stdout as a string
+    ps = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return ps.communicate()[0].decode('utf-8')
+
+
+# Script entry point
+
+while True:
+    print("Polling agent:")
+    data = agent.poll(10)
+    for i in data:
+        i["time"] = str(datetime.now()) # Get time at the manager to account for transmission time
+
+    print("Generating graph....", end="")
+    with open('MIB.txt', 'w') as MIB:
+        json.dump(data, MIB)
+    graphing.genGraph("MIB.txt")
+    print("done")
+
+    print("Sleeping...")
+    time.sleep(500)
