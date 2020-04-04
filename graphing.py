c63870ad904d7b9626c0d395d4960df676444484
Thomas Yungblut <sifotes@hotmail.com>
2020-04-04 15:55:40 -0400

    Now generating one graph per tunnel. Read all the information per tunnel, graph, repeat.

--- graphing.py
+++ graphing.py
@@ -1,36 +1,39 @@
-import json
 import matplotlib.pyplot as plt
-
 
 # Get all data from file.
 def genGraph(data):
-	tuns = []
-	for tun in data:
-		tuns.append({tun: {
-				"time": list(i["time"] for i in data[tun]),
-				"latency": list(i["latency"] for i in data[tun]),
-				"download": list(i["download"] for i in data[tun]),
-				"upload": list(i["upload"] for i in data[tun])
-			}
-		})
+  tuns = {}
+  fig, axes = plt.subplots(figsize=(12,8))
+  
 
-	fig, axes = plt.subplots(2, 1)
+  for tun in data:
+    print("\nTunnel Number: " + str(tun))
+    tuns.update({tun: {
+      "time": list(i["time"] for i in data[tun]),
+      "latency": list(i["latency"] for i in data[tun]),
+      "download": list(i["download"] for i in data[tun]),
+      "upload": list(i["upload"] for i in data[tun])
+    }})
+    fig, axes = plt.subplots(2, 1)
+    # reduce the number of x ticks. Not working
+    # axes.locator_params(nbins=5)
+    fig.suptitle("Recorded Stats: {}".format(tun))
+    fig.tight_layout(pad=5.0)
+    
+    axes[0].set_title('Speeds')
+    axes[0].plot(tuns[tun]["time"], tuns[tun]["download"])
+    axes[0].plot(tuns[tun]["time"], tuns[tun]["upload"])
+    axes[0].set_ylabel('Speed (Mbps)')
 
-	axes[0].set_title('Recorded Speeds')
-	axes[0].plot(times, download)
+    axes[1].set_title('Latency')
+    axes[1].plot(tuns[tun]["time"], tuns[tun]["latency"])
+    axes[1].set_ylabel('Latency (ms)')
 
-	axes[0].plot(times, upload)
-	axes[0].set_ylabel('Speed (Mbps)')
-
-
-	axes[1].set_title('Recorded Latency')
-	axes[1].plot(times, latency)
-	axes[1].set_ylabel('Latency (ms)')
-
-
-	#Enable legend titles
-	axes[0].legend(['Download', 'Upload'])
-	axes[1].legend(['Latency'])
-
-	#Generate Visual
-	plt.savefig('graph.png')
+    # Enable legend titles
+    axes[0].legend(['Download', 'Upload'])
+    axes[1].legend(['Latency'])
+    
+    # Generate Visual
+  # plt.show()
+    plt.savefig('Graphs/graph-{0}.png'.format(tun))
+    
